5fbb318ed12af3f9e66ce02a8823e2dcc78011e9
temporaryforfzuse <temporaryforfzuse@zsxsoft.com>
2019-03-09 17:25:02 +0800

    First commit

---
+++ 221600131&221600439/src/Core/Core.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "stdafx.h"
+#define EXTERN extern "C" __declspec(dllexport)
+
+constexpr auto WORDCOUNTRESULT_SUCCEED = 0;
+
+struct WordCountWordAppear {
+	char *word;
+	size_t count;
+};
+
+
+struct WordCountResult {
+	size_t errorCode = WORDCOUNTRESULT_SUCCEED;
+	size_t lines = 0;
+	size_t words = 0;
+	size_t characters = 0;
+	size_t uniqueWords = 0;
+	struct WordCountWordAppear *wordAppears;
+};
+
+
+EXTERN WordCountResult CalculateWordCount(const char* fileName);
+EXTERN void ClearWordAppear(WordCountResult* resultStruct);
+
+#undef EXTERN
